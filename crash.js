14ed12270d73df4bbb8c02d9e72aa78bbfb1158c
egimoro <gimoremma@gmail.com>
2019-06-12 23:57:30 +0200

    first js

---
+++ crash.js
@@ -0,0 +1,30 @@
+// variables (let, const)
+
+
+// datatypes (String, Numbers, Boolean, null, undefined)
+
+const name = 'Gaius';
+
+const age = 28;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+
+// Concatenation
+console.log('My name is '+name+' and I am '+ age)
+//Template String
+console.log(`My name is ${name} and I am ${age}`);
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello)
+
+
+
