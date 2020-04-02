b1a82dd8eeafbe6470e1bb486035c48e4b418718
egimoro <gimoremma@gmail.com>
2020-04-02 23:32:32 +0100

    For loops

--- loop.js
+++ loop.js
@@ -1,9 +1,3 @@
-let a = 0;
-while(a<10){
-    console.log('Block of code a is '+a);
-    a++;
-}
-
 let b = 20;
 do{
     console.log('Do loop b is '+b);
@@ -16,4 +10,24 @@ let c=0;
 while(myArray[c]){
     console.log(myArray[c]);
     c++; 
+}
+
+for(let a=0;a<10;a++){
+    console.log('a is '+a);
+}
+
+for(let b=0;b<myArray.length;b++){
+    console.log(myArray[b]);
+}
+
+let myObj= {firstName:"Weru", lastName:"Flegis", age:29};
+
+for(let x in myObj){
+    console.log(myObj[x]);
+    console.log(x);
+}
+
+let myStr = "JavaScript Course";
+for(let letter of myStr){
+    console.log(letter);
 }
