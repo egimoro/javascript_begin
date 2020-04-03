7335b912b62b189160aacf41b59e2432dd904f76
egimoro <gimoremma@gmail.com>
2020-04-03 22:50:58 +0100

    html output

--- loop.js
+++ loop.js
@@ -6,6 +6,11 @@ do{
 while(b<10);
 
 let myArray = ["Hello", "world",6,2,233,4771];
+
+const output = document.getElementById('output');
+output.innerHTML = "NEW";
+console.dir(output);
+
 let c=0;
 while(myArray[c]){
     console.log(myArray[c]);
@@ -31,3 +36,35 @@ let myStr = "JavaScript Course";
 for(let letter of myStr){
     console.log(letter);
 }
+
+let myArr2 = myArray.map(function(val){
+    console.log(val);
+    return typeof val;
+
+});
+
+console.log(myArr2);
+
+let myNumbs = [32,5444,23322,55,3232,3232,322,33];
+const newNumbs = myNumbs.map(function(x){
+    return x*3;
+});
+console.log(newNumbs);
+
+const newNumbs2 = myNumbs.map(x=>x*3);
+console.log(newNumbs);
+
+
+let MyArr = ["Hello", "World", 6, 2, 55, 2332, 3434];
+
+const output1 = document.getElementById('output1')
+let html = MyArr.map(function(value,index){
+    console.log(value);
+    console.log(index);  
+    return '<div>'+index+' '+value+'</div>';
+
+});
+let myStr1 = html.join('');
+output1.innerHTML = myStr1;
+console.log(myStr1);
+console.log(html);
